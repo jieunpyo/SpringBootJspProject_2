8031f4e1d1332aac58b7c3220b2e6697681ca511
jieunpyo <jieunhye1121@naver.com>
2025-12-05 16:04:25 +0900

    1111

---
+++ src/main/java/com/sist/web/SpringBootJspProject2Application.java
@@ -0,0 +1,26 @@
+package com.sist.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+ * 	 해당 폴더로 이동
+ * 	 git init
+ * 	 git add .
+ * 	 git remote add origin git주소
+ * 	 git branch -M main
+ * 	 git commit -m "1111"
+ * 	 git push -u origin main --force
+ * 
+ * 	 --------------------------------
+ * 	 git add .
+ * 	 git commit -m "2222"
+ * 	 git push -u origin main
+ */
+@SpringBootApplication
+public class SpringBootJspProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJspProject2Application.class, args);
+	}
+
+}
